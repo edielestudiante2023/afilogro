fb664e80c4a3d9ce51725e2ef4d6335822f98f42
edielestudiante2023 <edielestudiante2023@gmail.com>
2025-07-16 08:41:36 -0500

    boton de cerrar sesion

--- app/Views/admin/admindashboard.php
+++ app/Views/admin/admindashboard.php
@@ -47,7 +47,7 @@
     </div>
 </div>
 
-<?= $this->include('partials/logout') ?>
+
 
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
